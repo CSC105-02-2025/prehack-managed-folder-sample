459a2696e568339a9b957fb2eff15c789187c730
yephonekyaw <yephonekyaw231202@gmail.com>
2026-05-12 10:37:45 +0700

    feat: add addition feature module with full frontend and backend structure

--- backend/src/routers.ts
+++ backend/src/routers.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import additionRouter from "./modules/addition/routers/addition.router";
 
 const mainRouter = Router();
+
+mainRouter.use("/addition", additionRouter);
 
 export default mainRouter;
